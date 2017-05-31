378d5e2d674ed8b881a84f123fdf5b608f31855a
schoeu <schoeu89@gmail.com>
2017-06-01 00:11:00 +0800

    delete scripts & styles

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,8 @@
  * Main JS file for kaldorei behaviours
  */
 
+var $window = $(window);
+
 /* globals jQuery, document */
 (function($, undefined) {
     "use strict";
@@ -20,7 +22,7 @@
         });
 
         var timer = null;
-        $(window).scroll(function() {
+        $window.scroll(function() {
             if (timer) {
                 clearTimeout(timer);
                 timer = null;
@@ -41,7 +43,7 @@
 
         // #backTop Button Event
         $("#backTop").on("click", function() {
-            scrollToTop();
+            $window.scrollTop(0);
         });
 
         // highlight config
@@ -69,52 +71,20 @@
         if (toc.children().length == 0) $(".widget-toc").hide();
 
         var tocHieght = toc.height();
-        var tocFixedHeight = $(window).height() - 192;
+        var tocFixedHeight = $window.height() - 192;
         tocHieght > tocFixedHeight ?
             toc.css('height', tocFixedHeight) :
             toc.css('height', tocHieght)
 
-        $(window).resize(function() {
+        $window.resize(function() {
             var tocFixedHeight = $(this).height() - 192;
             tocHieght > tocFixedHeight ?
                 toc.css('height', tocFixedHeight) :
                 toc.css('height', tocHieght)
-        })
-
-        // toc animate effect
-        // bind click event to all internal page anchors
-        /*$('a.data-scroll').on('click', function(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            var target = $(this).attr('href');
-            $(target).velocity('scroll', {
-                duration: 500,
-                easing: 'ease-in-out'
-            });
-        });*/
+        });
 
         // tooltip config
         $('[data-rel=tooltip]').tooltip();
-
-        // fancybox config
-        /*$('.post-content a:has(img)').addClass('fancybox');
-        $(".fancybox").attr('rel', 'gallery-group').fancybox({
-            helpers: {
-                overlay: {
-                    css: {
-                        'background': 'rgba(0, 154, 97, 0.33)'
-                    },
-                    locked: false
-                }
-            },
-            beforeShow: function() {
-                var alt = this.element.find('img').attr('alt');
-
-                this.inner.find('img').attr('alt', alt);
-
-                this.title = alt;
-            }
-        });*/
 
         // add archives year
         var yearArray = new Array();
@@ -132,55 +102,4 @@
         }
     });
 
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    /*$.fn.arctic_scroll = function(options) {
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
-
-        allOptions = $.extend(defaults, options);
-
-        allOptions.elem.click(function(event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
-
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({
-                    scrollTop: ($(this.hash).offset().top + toMove)
-                }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({
-                    scrollTop: toMove
-                }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({
-                    scrollTop: ($(this.hash).offset().top)
-                }, allOptions.speed);
-            }
-        });
-    };*/
 })(jQuery);
-
-function scrollToTop(name, speed) {
-    $(window).scrollTop(0);
-    /*if (!speed) speed = 300
-    if (!name) {
-        $('html,body').animate({
-            scrollTop: 0
-        }, speed)
-    } else {
-        if ($(name).length > 0) {
-            $('html,body').animate({
-                scrollTop: $(name).offset().top
-            }, speed)
-        }
-    }*/
-}
